de2e42943864fb68ead79ddea5f4028391701fbc
Abby Chau <abzchau@gmail.com>
2021-04-28 23:14:29 +0000

    Fixes CRUD; Now DB, CRUD, Server Works

--- crud.py
+++ crud.py
@@ -1,6 +1,8 @@
+"""CRUD operations for the writing app"""
 
+from model import db, User, connect_to_db
 
-def create_user(email, password):
+def create_user(first_name, last_name, email, password, favorite_writer, favorite_animal):
     """Create and return a new user"""
 
     user = User(first_name=first_name, last_name=last_name, email=email, password=password, favorite_writer=favorite_writer, favorite_animal=favorite_animal)
@@ -9,3 +11,8 @@ def create_user(email, password):
     db.session.commit()
 
     return user
+
+
+if __name__=='__main__':
+    from server import app
+    connect_to_db(app)
