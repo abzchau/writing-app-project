056f949459648474a322d0b28513cb797943b870
Abby Chau <abzchau@gmail.com>
2021-04-29 01:10:39 +0000

    Fixes Test For Create User Using GitHub Actions

--- test_app.py
+++ test_app.py
@@ -1,7 +1,13 @@
 from crud import create_user
+from model import connect_to_db
 
 def test_create_user(user):
-    return create_user(first_name='mae', last_name='wong', email='mae@wong.com', password='test1', favorite_writer='Lessing', favorite_animal='dogs')
+    connect_to_db(app)
+    user = create_user(first_name='mae', last_name='wong', email='mae@wong.com', password='test1', favorite_writer='Lessing', favorite_animal='dogs')
+    return user
 
 def test_capitalize_string():
-    assert test_create_user() == '<User user_id=2 first_name=mae last_name=wong>'
+    assert test_create_user(user) == '<User user_id=2 first_name=mae last_name=wong>'
+
+
+
